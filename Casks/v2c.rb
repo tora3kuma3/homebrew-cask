8e1d8ba8b0044574f0fa02d858aa9f4fc5e7e8ed
tora3kuma3 <tora3kuma3@gmail.com>
2014-12-14 22:19:29 +0900

    Create v2c.rb

---
+++ Casks/v2c.rb
@@ -0,0 +1,8 @@
+cask :v1 => 'v2c' do
+  version '2.11.4'
+  sha256 '8c9264e633d25ed5d3fc256664ed62b1b8b23ea75c3e6f1146671cb8086591b2'
+  url 'http://v2c.s50.xrea.com/V2C_2.11.4_Mac.tar.gz'
+  homepage 'http://v2c.s50.xrea.com/'
+  license :unknown
+  app 'V2C.app'
+end
